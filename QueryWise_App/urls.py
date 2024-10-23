6743b398be956e957a7619eb91e4182397c6b341
Ezzz-Lui <ogn.lui@gmail.com>
2024-10-22 22:43:54 -0600

    add: set url config for auth

--- QueryWise_App/urls.py
+++ QueryWise_App/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('oauth2/', include('django_auth_adfs.urls')),
 ]
