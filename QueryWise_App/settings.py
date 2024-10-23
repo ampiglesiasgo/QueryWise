69578e7599f0d419141df413fee4fb8a3c977db4
Ezzz-Lui <ogn.lui@gmail.com>
2024-10-22 22:37:05 -0600

    add: config url redirect login

--- QueryWise_App/settings.py
+++ QueryWise_App/settings.py
@@ -122,3 +122,11 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTHENTICATION_BACKENDS = (
+    'django_auth_adfs.backend.AdfsAuthCodeBackend'
+)
+
+# Configure django to redirect users to the right URL for login
+LOGIN_URL = "django_auth_adfs:login"
+LOGIN_REDIRECT_URL = "/"
