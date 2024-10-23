8ca7f998ed468203395b99fde48fad3f986896dc
Ezzz-Lui <ogn.lui@gmail.com>
2024-10-23 16:47:39 -0600

    add: set default rest config

--- QueryWise_App/settings.py
+++ QueryWise_App/settings.py
@@ -162,3 +162,11 @@ AUTH_ADFS = {
     'TENANT_ID': tenant_id,
     'RELYING_PARTY_ID': client_id,
 }
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
