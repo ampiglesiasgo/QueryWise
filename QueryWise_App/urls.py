a2a5ec85bdb0f45cb6dc713701fe18c45de25765
Ezzz-Lui <ogn.lui@gmail.com>
2024-10-23 02:58:33 -0600

    add: set urls for app module

--- QueryWise_App/urls.py
+++ QueryWise_App/urls.py
@@ -21,4 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('oauth2/', include('django_auth_adfs.urls')),
+    path("", include("app.urls")),
 ]
