96d64dafe1e86597476b1ae50e8ce3bf8ce48b65
Ezzz-Lui <ogn.lui@gmail.com>
2024-10-26 10:47:24 -0600

    refactor: refactor project structure on django api

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./templates/**/*.html','./templates/**/*.twig'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
