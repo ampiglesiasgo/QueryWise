5e0e6ac6fb90ecc9ac24555e794d8c89079339a6
Luis Ramos <ogn.lui@gmail.com>
2024-10-31 01:28:29 -0600

    Delete django_api directory for not use

--- django_api/api_django/api/serializers.py
+++
@@ -1,5 +0,0 @@
-from rest_framework import serializers
-
-class FilePromptSerializer(serializers.Serializer):
-    file = serializers.FileField()
-    prompt = serializers.CharField(max_length=500)
--- django_api/api_django/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import test_api, process_document_and_prompt
-
-urlpatterns = [
-    path('test/', test_api, name='test_api'),
-    path('process/', process_document_and_prompt, name='process_document_and_prompt')
-]
